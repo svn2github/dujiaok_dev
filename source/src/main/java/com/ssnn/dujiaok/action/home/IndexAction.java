b28e9f1a702de73360f8a521849eb6399a38638d
langbenlanglou@gmail.com <langbenlanglou@gmail.com@277dfb4e-25bd-d8e7-4fa6-b42a7a2d9503>
2011-12-16 17:44:42 +0000

    struts框架代码
    git-svn-id: http://koaijud.googlecode.com/svn/trunk@6 277dfb4e-25bd-d8e7-4fa6-b42a7a2d9503

---
+++ source/src/main/java/com/ssnn/dujiaok/action/home/IndexAction.java
@@ -0,0 +1,11 @@
+package com.ssnn.dujiaok.action.home;
+
+import com.opensymphony.xwork2.ActionSupport;
+
+public class IndexAction extends ActionSupport {
+
+	@Override
+	public String execute() throws Exception {
+		return SUCCESS;
+	}
+}
