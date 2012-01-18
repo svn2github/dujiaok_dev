3f4424c523497dab594780ec8a855d828383c30a
langbenlanglou@gmail.com <langbenlanglou@gmail.com@277dfb4e-25bd-d8e7-4fa6-b42a7a2d9503>
2012-01-18 10:49:19 +0000

    git-svn-id: http://koaijud.googlecode.com/svn/trunk@63 277dfb4e-25bd-d8e7-4fa6-b42a7a2d9503

--- source/src/main/java/com/ssnn/dujiaok/web/action/admin/HotelAction.java
+++ source/src/main/java/com/ssnn/dujiaok/web/action/admin/HotelAction.java
@@ -1,10 +1,13 @@
 package com.ssnn.dujiaok.web.action.admin;
 
+import java.util.List;
+
 import org.apache.commons.lang.StringUtils;
 
 import com.opensymphony.xwork2.ModelDriven;
 import com.ssnn.dujiaok.biz.service.HotelService;
 import com.ssnn.dujiaok.model.HotelDO;
+import com.ssnn.dujiaok.util.StringListConventUtil;
 import com.ssnn.dujiaok.web.action.BasicAction;
 
 /**
@@ -16,12 +19,17 @@ public class HotelAction extends BasicAction implements ModelDriven<HotelDO>{
 
 	private HotelDO hotel = new HotelDO() ;
 	
+	private List<String> imagesList ;
+	
 	private HotelService hotelService ;
 	
 	@Override
 	public String execute() throws Exception {
 		if(hotel!=null && StringUtils.isNotBlank(hotel.getHotelId())){
 			hotel = hotelService.getHotel(hotel.getHotelId()) ;
+			if(hotel != null){
+				imagesList = StringListConventUtil.toList(hotel.getImages()) ;
+			}
 		}
 		return SUCCESS ;
 	}
@@ -32,6 +40,7 @@ public class HotelAction extends BasicAction implements ModelDriven<HotelDO>{
 	 * @throws Exception
 	 */
 	public String create() throws Exception {
+		hotel.setImages(StringListConventUtil.toString(imagesList)) ;
 		if(StringUtils.isBlank(hotel.getHotelId())){
 			hotel = hotelService.createHotel(hotel) ;
 		}else{
@@ -55,6 +64,14 @@ public class HotelAction extends BasicAction implements ModelDriven<HotelDO>{
 	
 	public HotelDO getHotel() {
 		return hotel;
+	}
+
+	public List<String> getImagesList() {
+		return imagesList;
+	}
+
+	public void setImagesList(List<String> imagesList) {
+		this.imagesList = imagesList;
 	}
 
 	public void setHotel(HotelDO hotel) {
