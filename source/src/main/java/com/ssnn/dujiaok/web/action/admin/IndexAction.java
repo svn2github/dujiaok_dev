5dc7cbddfad7d8a7b625301f5e8ea5fc50c7683c
langbenlanglou@gmail.com <langbenlanglou@gmail.com@277dfb4e-25bd-d8e7-4fa6-b42a7a2d9503>
2012-02-01 13:15:47 +0000

    git-svn-id: http://koaijud.googlecode.com/svn/trunk@73 277dfb4e-25bd-d8e7-4fa6-b42a7a2d9503

---
+++ source/src/main/java/com/ssnn/dujiaok/web/action/admin/IndexAction.java
@@ -0,0 +1,12 @@
+package com.ssnn.dujiaok.web.action.admin;
+
+import com.ssnn.dujiaok.web.action.BasicAction;
+
+@SuppressWarnings("serial")
+public class IndexAction extends BasicAction {
+
+	@Override
+	public String execute() throws Exception {
+		return SUCCESS ;
+	}
+}
