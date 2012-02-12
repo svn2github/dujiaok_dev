c08007d1f21898c5744a61abc7f68f900caf0ddf
ib20111111@gmail.com <ib20111111@gmail.com@277dfb4e-25bd-d8e7-4fa6-b42a7a2d9503>
2012-02-12 12:16:34 +0000

    git-svn-id: http://koaijud.googlecode.com/svn/trunk@104 277dfb4e-25bd-d8e7-4fa6-b42a7a2d9503

--- source/src/main/java/com/ssnn/dujiaok/constant/IndexChannelConstants.java
+++ source/src/main/java/com/ssnn/dujiaok/constant/IndexChannelConstants.java
@@ -15,107 +15,107 @@ public abstract class IndexChannelConstants {
     /**
      * A1每日特价模块key
      */
-    public static final String MODULE_TODAY_SPECIALS                  = "index.yoday.specials.module";
+    public static final String M_TODAY_SPECIALS                  = "index.yoday.specials.module";
     /**
      * A2广告模块key（顶部左边，爱玩水爱夏天）
      */
-    public static final String MODULE_IMAGE_ADS_A2                    = "index.image.ads.A2.module";
+    public static final String M_IMAGE_ADS_A2                    = "index.image.ads.A2.module";
 
     /**
      * A3 banner模块key
      */
-    public static final String MODULE_BANNER                          = "index.banner.module";
+    public static final String M_BANNER                          = "index.banner.module";
     /**
      * A4搜索主题模块key
      */
-    public static final String MODULE_SEARCH_THEME                    = "index.search.theme.module";
+    public static final String M_SEARCH_THEME                    = "index.search.theme.module";
     /**
      * A4搜索景点模块key
      */
-    public static final String MODULE_SEARCH_ATTRACTIONS              = "index.search.attractions.module";
+    public static final String M_SEARCH_ATTRACTIONS              = "index.search.attractions.module";
     /**
      * A4搜索周边模块key
      */
-    public static final String MODULE_SEARCH_PERIPHERY                = "index.search.periphery.module";
+    public static final String M_SEARCH_PERIPHERY                = "index.search.periphery.module";
     /**
      * A4搜索价位模块key
      */
-    public static final String MODULE_SEARCH_PRICE                    = "index.search.price.module";
+    public static final String M_SEARCH_PRICE                    = "index.search.price.module";
 
     /**
      *A5广告有模块key（顶部右边，限时抢购）
      */
-    public static final String MODULE_IMAGE_ADS_A5                    = "index.image.ads.A5.module";
+    public static final String M_IMAGE_ADS_A5                    = "index.image.ads.A5.module";
     /**
      * B广告有模块key（1F上面，六月抢杨梅咯）
      */
-    public static final String MODULE_IMAGE_ADS_B                     = "index.image.ads.B.module";
+    public static final String M_IMAGE_ADS_B                     = "index.image.ads.B.module";
 
     /**
      * C1广告有模块key（周边自助左上角，千岛碧水）
      */
-    public static final String MODULE_IMAGE_ADS_C1                    = "index.image.ads.C1.module";
+    public static final String M_IMAGE_ADS_C1                    = "index.image.ads.C1.module";
 
     /**
      * C2周边自助文字推荐
      */
-    public static final String MODULE_PERIPHERY_TEXT_RECOMMEND        = "index.periphery.text.recommend.module";
+    public static final String M_PERIPHERY_TEXT_RECOMMEND        = "index.periphery.text.recommend.module";
 
     /**
      * C3.1广告有模块key（周边自助右上角，游秦淮）
      */
-    public static final String MODULE_IMAGE_ADS_C3_1                  = "index.image.ads.C3.1.module";
+    public static final String M_IMAGE_ADS_C3_1                  = "index.image.ads.C3.1.module";
     /**
      * C3.2广告有模块key（周边自助右上角，杭州江南）
      */
-    public static final String MODULE_IMAGE_ADS_C3_2                  = "index.image.ads.C3.2.module";
+    public static final String M_IMAGE_ADS_C3_2                  = "index.image.ads.C3.2.module";
     /**
      * C4周边自助图片推荐
      */
-    public static final String MODULE_PERIPHERY_IMAGE_RECOMMEND       = "index.periphery.image.recommend.module";
+    public static final String M_PERIPHERY_IMAGE_RECOMMEND       = "index.periphery.image.recommend.module";
     /**
      * C5周边自助景点推荐
      */
-    public static final String MODULE_PERIPHERY_ATTRACTIONS_RECOMMEND = "index.periphery.attractions.recommend.module";
+    public static final String M_PERIPHERY_ATTRACTIONS_RECOMMEND = "index.periphery.attractions.recommend.module";
 
     /**
      * D1广告有模块key（周边自助左上角，千岛碧水）
      */
-    public static final String MODULE_IMAGE_ADS_D1                    = "index.image.ads.D1.module";
+    public static final String M_IMAGE_ADS_D1                    = "index.image.ads.D1.module";
 
     /**
      * D2周边自助文字推荐
      */
-    public static final String MODULE_LONG_TERM_TEXT_RECOMMEND        = "index.long-term.text.recommend.module";
+    public static final String M_LONG_TERM_TEXT_RECOMMEND        = "index.long-term.text.recommend.module";
 
     /**
      * D2.1广告有模块key（周边自助右上角，游秦淮）
      */
-    public static final String MODULE_IMAGE_ADS_D3_1                  = "index.image.ads.D3.1.module";
+    public static final String M_IMAGE_ADS_D3_1                  = "index.image.ads.D3.1.module";
     /**
      * D3.2广告有模块key（周边自助右上角，杭州，江南）
      */
-    public static final String MODULE_IMAGE_ADS_D3_2                  = "index.image.ads.D3.2.module";
+    public static final String M_IMAGE_ADS_D3_2                  = "index.image.ads.D3.2.module";
     /**
      * D4周边自助图片推荐
      */
-    public static final String MODULE_LONG_TERM_IMAGE_RECOMMEND       = "index.long-term.image.recommend.module";
+    public static final String M_LONG_TERM_IMAGE_RECOMMEND       = "index.long-term.image.recommend.module";
     /**
      * D5周边自助景点推荐
      */
-    public static final String MODULE_LONG_TERM_ATTRACTIONS_RECOMMEND = "index.long-term.attractions.recommend.module";
+    public static final String M_LONG_TERM_ATTRACTIONS_RECOMMEND = "index.long-term.attractions.recommend.module";
 
     /**
      * E1.1度假ok动态top
      */
-    public static final String MODULE_DUJIAOK_DYNAMIC_TOP             = "index.dujiaok.dynamic.top.module";
+    public static final String M_DUJIAOK_DYNAMIC_TOP             = "index.dujiaok.dynamic.top.module";
     /**
      * E1.2度假ok动态
      */
-    public static final String MODULE_DUJIAOK_DYNAMIC                 = "index.dujiaok.dynamic.module";
+    public static final String M_DUJIAOK_DYNAMIC                 = "index.dujiaok.dynamic.module";
     /**
      * E2驴友热议
      */
-    public static final String MODULE_TRAVEL_MATE_DYNAMIC             = "index.travel.mate.module";
+    public static final String M_TRAVEL_MATE_DYNAMIC             = "index.travel.mate.module";
 
 }
