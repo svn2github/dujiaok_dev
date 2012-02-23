ac86841b2126b56505475204032bda29bd2579c2
ib20111111@gmail.com <ib20111111@gmail.com@277dfb4e-25bd-d8e7-4fa6-b42a7a2d9503>
2012-02-23 17:28:40 +0000

    add
    git-svn-id: http://koaijud.googlecode.com/svn/trunk@179 277dfb4e-25bd-d8e7-4fa6-b42a7a2d9503

---
+++ source/webapp/src/main/java/com/ssnn/dujiaok/web/velocity/toolbox/FrontConstants.java
@@ -0,0 +1,19 @@
+package com.ssnn.dujiaok.web.velocity.toolbox;
+
+import com.ssnn.dujiaok.constant.IndexChannelConstants;
+
+/**
+ * @author ib 2012-2-22 上午01:17:09
+ */
+public class FrontConstants {
+    public IndexChannelConstants indexChannel = new IndexChannelConstants();
+    
+    public IndexChannelConstants getIndexChannel() {
+        return indexChannel;
+    }
+    
+    public void setIndexChannel(IndexChannelConstants indexChannel) {
+        this.indexChannel = indexChannel;
+    }
+    
+}
