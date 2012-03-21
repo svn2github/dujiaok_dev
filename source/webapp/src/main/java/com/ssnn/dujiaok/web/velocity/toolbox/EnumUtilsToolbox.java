923cb09fbbc296792996b9d6128a61c1876ae626
langbenlanglou@gmail.com <langbenlanglou@gmail.com@277dfb4e-25bd-d8e7-4fa6-b42a7a2d9503>
2012-03-21 17:23:58 +0000

    git-svn-id: http://koaijud.googlecode.com/svn/trunk@234 277dfb4e-25bd-d8e7-4fa6-b42a7a2d9503

--- source/webapp/src/main/java/com/ssnn/dujiaok/web/velocity/toolbox/EnumUtilsToolbox.java
+++ source/webapp/src/main/java/com/ssnn/dujiaok/web/velocity/toolbox/EnumUtilsToolbox.java
@@ -1,5 +1,10 @@
 package com.ssnn.dujiaok.web.velocity.toolbox;
 
+import java.util.List;
+
+import org.apache.commons.lang.StringUtils;
+
+import com.ssnn.dujiaok.util.ArrayStringUtils;
 import com.ssnn.dujiaok.util.enums.OrderStatusEnums;
 import com.ssnn.dujiaok.util.enums.PayStatusEnums;
 import com.ssnn.dujiaok.util.enums.PayTypeEnums;
@@ -39,5 +44,15 @@ public class EnumUtilsToolbox {
 		return ProductEnums.fromValue(name) ;
 	}
 	
-	
+	public String toPayTypeEnumDescs(String nameListString) {
+		List<String> payTypeEnums = ArrayStringUtils.toList(nameListString) ;
+		String desc = "" ;
+		for(String name : payTypeEnums){
+			desc += PayTypeEnums.fromValue(name).getDesc() + "," ;
+		}
+		if(desc.length() > 0){
+			desc = StringUtils.removeEnd(desc, ",") ;
+		}
+		return desc ;
+	}
 }
