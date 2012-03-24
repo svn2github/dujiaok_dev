f588f4eff8154f91d4cd4ea93bf93465f57dadd9
langbenlanglou@gmail.com <langbenlanglou@gmail.com@277dfb4e-25bd-d8e7-4fa6-b42a7a2d9503>
2012-03-24 12:57:02 +0000

    git-svn-id: http://koaijud.googlecode.com/svn/trunk@246 277dfb4e-25bd-d8e7-4fa6-b42a7a2d9503

--- static/dujiaok/js/search.js
+++ static/dujiaok/js/search.js
@@ -1,3 +1,30 @@
-$(function(){
-	ok.checkPage();
-})
+var _search = function(){
+	var e = document.getElementById('schIpt');
+	if(typeof(e) == 'undefined'){
+		return false ;
+	}
+	var t = e.value ; 
+	if(t == '' || typeof(t)=='undefined'){
+		alert('ÇëÊäÈëËÑË÷¹Ø¼ü×Ö');
+		return false ;
+	}
+	document.getElementById("searchForm").submit() ;
+}
+
+var _search_p = function(product){
+	if(product!=null && product!='' && typeof(product)!='undefined'){
+		document.getElementById('s_product').value = product ;
+	}else{
+		document.getElementById('s_product').value = '' ;
+	}
+	document.getElementById("searchForm").submit() ;
+}
+
+var _search_d = function(days){
+	if(days!=null && typeof(days)!='undefined'){
+		document.getElementById('s_days').value = days ;
+	}else{
+		document.getElementById('s_days').value = '' ;
+	}
+	document.getElementById("searchForm").submit() ;
+}
