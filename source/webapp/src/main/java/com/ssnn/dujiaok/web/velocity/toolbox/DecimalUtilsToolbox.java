da0bba77cd82c2d8bd038876ac516e0be526193e
langbenlanglou@gmail.com <langbenlanglou@gmail.com@277dfb4e-25bd-d8e7-4fa6-b42a7a2d9503>
2012-04-14 13:09:35 +0000

    git-svn-id: http://koaijud.googlecode.com/svn/trunk@282 277dfb4e-25bd-d8e7-4fa6-b42a7a2d9503

---
+++ source/webapp/src/main/java/com/ssnn/dujiaok/web/velocity/toolbox/DecimalUtilsToolbox.java
@@ -0,0 +1,77 @@
+package com.ssnn.dujiaok.web.velocity.toolbox;
+
+import java.math.BigDecimal;
+import java.text.DecimalFormat;
+
+import org.apache.commons.lang.StringUtils;
+import org.apache.commons.lang.math.NumberUtils;
+
+public class DecimalUtilsToolbox {
+
+	
+	public String format (BigDecimal decimal , String pattern){
+		if(decimal == null){
+			return "" ;
+		}
+		DecimalFormat f = new DecimalFormat(pattern) ;
+		return f.format(decimal) ;
+	}
+	
+	/**
+	 * ֻ���С��
+	 * @param decimal
+	 * @param pattern
+	 * @return
+	 */
+	public String fractionFormat (BigDecimal decimal , String pattern){
+		if(decimal == null){
+			return "" ;
+		}
+		DecimalFormat f = new DecimalFormat(pattern) ;
+		f.setMaximumIntegerDigits(0) ;
+		return f.format(decimal) ;
+	}
+	
+	public BigDecimal divide(BigDecimal d1 , String decimalStr){
+		return d1.divide(new BigDecimal(decimalStr)) ;
+	}
+	
+	/**
+	 * createInt
+	 * @param str
+	 * @return
+	 */
+	public int createInt(String str){
+		if(StringUtils.isBlank(str)){
+			return 0 ;
+		}
+		return NumberUtils.createInteger(str) ;
+	}
+	
+	public int createInt(Integer i){
+		if(i == null){
+			return 0 ;
+		}
+		return i ;
+	}
+	
+	public int createInt(Integer i , int defaultVal){
+		if(i == null){
+			return defaultVal ;
+		}
+		return createInt(i) ;
+	}
+	
+	/**
+	 * createInt
+	 * @param str
+	 * @return
+	 */
+	public int createInt(String str ,int defaultVal ){
+		if(StringUtils.isBlank(str)){
+			return defaultVal ;
+		}
+		return createInt(str) ;
+	} 
+	
+}
