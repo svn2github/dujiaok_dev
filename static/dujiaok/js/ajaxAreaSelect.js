103314d180a90efee5bc100fa127fd1766dfba1f
langbenlanglou@gmail.com <langbenlanglou@gmail.com@277dfb4e-25bd-d8e7-4fa6-b42a7a2d9503>
2012-05-12 11:55:56 +0000

    git-svn-id: http://koaijud.googlecode.com/svn/trunk@365 277dfb4e-25bd-d8e7-4fa6-b42a7a2d9503

---
+++ static/dujiaok/js/ajaxAreaSelect.js
@@ -0,0 +1,97 @@
+
+var addBlankOption = function(id){
+	var o = new Option('','') ;
+	document.getElementById(id).options.add( o , 0) ;	
+	//o.selected = true ;
+}
+
+/**
+ *
+ * @param param 
+ * {id:id,url:url,type:type,parentName:parentName,name:name} 
+ * @param result 
+ * @returns
+ */
+var __setupAddressSelect = function(param,result){
+		var select = document.getElementById(param.id) ;
+		select.options.length = 0  ;
+		for(var i=0 ;i<result.length;i++){
+			var p = result[i] ;
+			var o = new Option(p.name,p.name) ;
+    		select.options.add(o) ;
+		}
+		var options = select.options ;
+		addBlankOption(param.id) ;
+		options[0].selected = true ;
+		if(typeof(param.name)!='undefined' && param.name!=''){
+    	for(var i=0 ;i<options.length;i++){
+    		if(options[i].value == param.name){
+    			options[i].selected = true ;
+    			break ;
+    		}
+    	}
+	}
+	
+}
+
+/**
+ * 
+ * {id:id,url:url,type:type,parentName:parentName,name:name} 
+ */
+var setupAddressSelect = function(p){
+	var select = document.getElementById(p.id) ;
+	select.options.length = 0  ;
+	var id = p.id;
+	var url = p.url ;
+	var type = p.type ;
+	var parentName = p.parentName ;
+	var name = p.name ;
+	var params = 'type='+type+'&name='+encodeURI(parentName)+'&t='+new Date() ;
+	var request = url + '?' + params ;
+	if(type!='province' && parentName==''){
+		return ;
+	}
+	xui.util.Ajax.asyncGET(request, 
+			function(data){
+				
+				try{
+					var result = eval('('+data+')') ;
+					__setupAddressSelect(p,result.result) ;
+				}catch(e){
+					alert('exception :' + e.message) ;
+				}
+			} , 
+			function(data){
+				alert("get data error!") ;
+			}
+	)
+}
+
+/**
+ * 
+ * @param url ${env.root}
+ * @param id documentId
+ * @param type 'province' | 'city' | 'area'
+ * @param name
+ * @param parentName
+ * @returns
+ */
+var initAddressSelect = function(url,id,type,name,parentName){
+	var p = {id:id,url:url,type:type,parentName:parentName,name:name} ;
+	setupAddressSelect(p) ;
+}
+
+var setupSelect = function(id,selectValue){
+	if(typeof(selectValue)=='undefined'){
+		return ;
+	}
+	var select = document.getElementById(id) ;
+	var options = select.options ;
+	for(var i=0 ;i<options.length;i++){
+		if(options[i].value == selectValue){
+			options[i].selected = true ;
+			break ;
+		}
+		
+	}
+}
