4ab8e491b2c8cbc54bb0fbad8548f170b2b8d824
langbenlanglou@gmail.com <langbenlanglou@gmail.com@277dfb4e-25bd-d8e7-4fa6-b42a7a2d9503>
2012-08-05 08:24:23 +0000

    git-svn-id: http://koaijud.googlecode.com/svn/trunk@494 277dfb4e-25bd-d8e7-4fa6-b42a7a2d9503

--- doc/maven-autoconf-plugin/src/main/java/com/alibaba/antx/config/ConfigRuntime.java
+++
@@ -1,5 +0,0 @@
-package com.alibaba.antx.config;
-
-public interface ConfigRuntime extends ConfigSettings {
-    void start();
-}
--- doc/maven-autoconf-plugin/src/main/java/com/alibaba/antx/config/gui/util/ConfiguratorConstant.java
+++
@@ -1,5 +0,0 @@
-package com.alibaba.antx.config.gui.util;
-
-public interface ConfiguratorConstant {
-    String[] PACKAGE_FILE_EXTS = new String[] { "*.jar", "*.war", "*.ear", "*.rar", "*.*" };
-}
